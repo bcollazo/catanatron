80292d86795dd04c50f72ca0dc28e106de6670c1
Bryan Collazo <bcollazo2010@gmail.com>
2020-08-15 02:18:11 -0400

    Fix numbers

--- models.py
+++ models.py
@@ -86,7 +86,7 @@ PORT_DECK = [
     Port(),
 ]
 
-NUMBERS_DECK = [2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 12]
+NUMBERS_DECK = [2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12]
 
 
 def generate_board():
