b15de62b42d6589461df6d942b4b0f0289208b3c
Bryan Collazo <bcollazo2010@gmail.com>
2020-09-25 10:15:50 -0400

    Add serialization test

---
+++ tests/test_json.py
@@ -0,0 +1,28 @@
+import json
+
+from catanatron.game import Game
+from catanatron.models.player import SimplePlayer, Color
+from catanatron.json import GameEncoder
+
+
+def test_serialization():
+    game = Game(
+        players=[
+            SimplePlayer(Color.RED),
+            SimplePlayer(Color.BLUE),
+            SimplePlayer(Color.WHITE),
+            SimplePlayer(Color.ORANGE),
+        ]
+    )
+    game.play_initial_build_phase()
+
+    string = json.dumps(game, cls=GameEncoder)
+    result = json.loads(string)
+
+    # Loosely assert looks like expected
+    assert len(result["players"]) == 4
+    assert isinstance(result["robber_coordinate"], list)
+    assert isinstance(result["tiles"], list)
+    assert isinstance(result["edges"], dict)
+    assert isinstance(result["nodes"], dict)
+    assert isinstance(result["actions"], list)
