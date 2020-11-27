cd098edb795985272550759cc02b08e26efb749e
Bryan Collazo <bcollazo2010@gmail.com>
2020-11-27 11:47:06 -0400

    ML: Correct features (hide enemy dev playable)

--- machine_learning/features.py
+++ machine_learning/features.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f65ff87a62ead330102862bed3ac5a48959ff917b56861052979fa0ef6721f9
-size 8172
+oid sha256:b420583aec1417f68988072f0e7a6760c768bba1ec7291aa77d67c5fc8b6860e
+size 8231
