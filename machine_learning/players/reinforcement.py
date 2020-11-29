c8d89afeb6d4e82d8c35b9a41d6aab4421dd6d3d
Bryan Collazo <bcollazo2010@gmail.com>
2020-11-28 20:54:17 -0400

    ML: Faster QRLPlayer

--- machine_learning/players/reinforcement.py
+++ machine_learning/players/reinforcement.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fec5cc4e5326bbde9297f722a4c8b18cad36e4e363a56b7549ad56d3758b09d1
-size 6527
+oid sha256:262c4e4c96fd982ead96a6e4e579422c499491d102be84189aaa66184d92ed30
+size 6281
