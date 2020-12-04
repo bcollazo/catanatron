31f61d5ac10835e8920f0d1c36ca4784ecdbceb9
Bryan Collazo <bcollazo2010@gmail.com>
2020-12-04 08:14:33 -0400

    ML: Small improvement. Make model serializable.

--- machine_learning/players/reinforcement.py
+++ machine_learning/players/reinforcement.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f09d5ccf80c789994cca34235b6a32b090dd16127b19c8d03a483f8d3928c2e2
-size 8004
+oid sha256:7088f6db4b7ec5b7778c72606d754084fcfff8037ec85393817ae494751558b2
+size 8089
