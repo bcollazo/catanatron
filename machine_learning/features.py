e884b86eff586d2cd828689418298e292998e81e
Bryan Collazo <bcollazo2010@gmail.com>
2020-12-12 12:52:31 -0400

    Correct expansion features

--- machine_learning/features.py
+++ machine_learning/features.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2f01f8e9b4efd4fa336e981acfdb74c55fc18d04557f08baa9cc149b0ae17ce
-size 11420
+oid sha256:2ce500a344e44e288837b7c14acff5f80632df518cf8b7a53a41f7ed29dbca16
+size 12614
