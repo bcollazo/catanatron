d03c3099e88064205403556922dfe67e325fd502
Bryan Collazo <bcollazo2010@gmail.com>
2020-12-25 15:09:09 -0400

    comment improvement

--- catanatron/models/player.py
+++ catanatron/models/player.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03e46cb620b8a8f00325fc9b438ae9cbcb4008da172fee8ec55514b1ebfbdb99
-size 3361
+oid sha256:99a20edf1a782457c0433d542bd4df3c51ad62725e8c7b9a93c294442325830c
+size 3363
