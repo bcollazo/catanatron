f828ce6a57624eebc4eb5793e8025f4844fec98a
Bryan Collazo <bcollazo2010@gmail.com>
2021-04-23 16:40:37 -0400

    Improve Robber Location

--- ui/src/pages/Robber.js
+++ ui/src/pages/Robber.js
@@ -7,7 +7,7 @@ export default function Robber({ center, size, coordinate }) {
   const [centerX, centerY] = center;
   const w = SQRT3 * size;
   const [tileX, tileY] = tilePixelVector(coordinate, size, centerX, centerY);
-  const [deltaX, deltaY] = [-w / 4, 0];
+  const [deltaX, deltaY] = [-w / 2 + w / 8, 0];
   const x = tileX + deltaX;
   const y = tileY + deltaY;
 
