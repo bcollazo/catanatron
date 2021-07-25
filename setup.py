f3da50efbcad71b15f344bdc24c35ec7490e7a9e
Bryan Collazo <bcollazo2010@gmail.com>
2021-07-25 14:59:38 -0400

    Bump version to 2.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="catanatron",
-    version="2.0.0",
+    version="2.1.0",
     author="Bryan Collazo",
     author_email="bcollazo2010@gmail.com",
     description="Fast Settlers of Catan Python Implementation",
