3a109929fba14929ed88c67cfcd95bacb18f3566
Bryan Collazo <bcollazo2010@gmail.com>
2021-07-30 12:58:14 -0400

    Fix catanatron_gym PyPi build

--- catanatron_core/setup.py
+++ catanatron_core/setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/bcollazo/catanatron",
-    packages=setuptools.find_packages(exclude=["catanatron_server"]),
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
