05d92d996fc7c8e95bfc523dd1469b59b6a48977
Bryan Collazo <bcollazo2010@gmail.com>
2022-01-15 11:57:33 -0400

    Allow Python3.6

--- catanatron_server/setup.py
+++ catanatron_server/setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.6",
     install_requires=[
         "catanatron",
         "flask",
