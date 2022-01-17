6f595e8cf54ce781317496f21f70545fe94bcc19
Bryan Collazo <bcollazo2010@gmail.com>
2022-01-16 23:19:32 -0400

    New console output and JSON outputs

---
+++ catanatron_experimental/catanatron_experimental/cli/accumulators.py
@@ -0,0 +1,213 @@
+import time
+import os
+import json
+from collections import defaultdict
+from catanatron.json import GameEncoder
+
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+
+from catanatron.game import Accumulator
+from catanatron.state_functions import get_actual_victory_points
+from catanatron_server.models import database_session, upsert_game_state
+from catanatron_server.utils import ensure_link
+from catanatron_experimental.utils import formatSecs
+from catanatron_experimental.machine_learning.utils import (
+    get_discounted_return,
+    get_tournament_return,
+    get_victory_points_return,
+    populate_matrices,
+    DISCOUNT_FACTOR,
+)
+from catanatron_gym.features import create_sample
+from catanatron_gym.envs.catanatron_env import to_action_space
+from catanatron_experimental.machine_learning.board_tensor_features import (
+    create_board_tensor,
+)
+
+
+class StatisticsAccumulator(Accumulator):
+    def __init__(self):
+        self.wins = defaultdict(int)
+        self.turns = []
+        self.ticks = []
+        self.durations = []
+        self.games = []
+        self.results_by_player = defaultdict(list)
+
+    def initialize(self, game):
+        self.start = time.time()
+
+    def finalize(self, game):
+        duration = time.time() - self.start
+        winning_color = game.winning_color()
+        if winning_color is None:
+            return  # do not track
+
+        self.wins[winning_color] += 1
+        self.turns.append(game.state.num_turns)
+        self.ticks.append(len(game.state.actions))
+        self.durations.append(duration)
+        self.games.append(game)
+
+        for player in game.state.players:
+            points = get_actual_victory_points(game.state, player.color)
+            self.results_by_player[player.color].append(points)
+
+    def get_avg_ticks(self):
+        return sum(self.ticks) / len(self.ticks)
+
+    def get_avg_turns(self):
+        return sum(self.turns) / len(self.turns)
+
+    def get_avg_duration(self):
+        return sum(self.durations) / len(self.durations)
+
+
+class StepDatabaseAccumulator(Accumulator):
+    """
+    Saves a game state to database for each tick.
+    Slows game ~1s per tick.
+    """
+
+    def initialize(self, game):
+        with database_session() as session:
+            upsert_game_state(game, session)
+
+    def step(game):
+        with database_session() as session:
+            upsert_game_state(game, session)
+
+
+class DatabaseAccumulator(Accumulator):
+    """Saves last game state to database"""
+
+    def finalize(self, game):
+        self.link = ensure_link(game)
+
+
+class JsonDataAccumulator(Accumulator):
+    def __init__(self, output):
+        self.output = output
+
+    def finalize(self, game):
+        # game_json = GameEncoder().default(game)
+        filepath = os.path.join(self.output, f"{game.id}.json")
+        with open(filepath, "w") as f:
+            f.write(json.dumps(game, cls=GameEncoder))
+
+        # wiet
+        # pprint(game_json)  # inspect a game state representation
+
+
+class CsvDataAccumulator(Accumulator):
+    def __init__(self, output):
+        self.output = output
+
+    def initialize(self, game):
+        self.data = defaultdict(
+            lambda: {"samples": [], "actions": [], "board_tensors": []}
+        )
+
+    def step(self, game, action):
+        self.data[action.color]["samples"].append(create_sample(game, action.color))
+        self.data[action.color]["actions"].append(to_action_space(action))
+        board_tensor = create_board_tensor(game, action.color)
+        shape = board_tensor.shape
+        flattened_tensor = tf.reshape(
+            board_tensor, (shape[0] * shape[1] * shape[2],)
+        ).numpy()
+        self.data[action.color]["board_tensors"].append(flattened_tensor)
+
+    def finalize(self, game):
+        if game.winning_color() is None:
+            return  # drop game
+
+        print("Flushing to matrices...")
+        t1 = time.time()
+        samples = []
+        actions = []
+        board_tensors = []
+        labels = []
+        for color in game.state.colors:
+            player_data = self.data[color]
+
+            # Make matrix of (RETURN, DISCOUNTED_RETURN, TOURNAMENT_RETURN, DISCOUNTED_TOURNAMENT_RETURN)
+            episode_return = get_discounted_return(game, color, 1)
+            discounted_return = get_discounted_return(game, color, DISCOUNT_FACTOR)
+            tournament_return = get_tournament_return(game, color, 1)
+            vp_return = get_victory_points_return(game, color)
+            discounted_tournament_return = get_tournament_return(
+                game, color, DISCOUNT_FACTOR
+            )
+
+            samples.extend(player_data["samples"])
+            actions.extend(player_data["actions"])
+            board_tensors.extend(player_data["board_tensors"])
+            return_matrix = np.tile(
+                [
+                    [
+                        episode_return,
+                        discounted_return,
+                        tournament_return,
+                        discounted_tournament_return,
+                        vp_return,
+                    ]
+                ],
+                (len(player_data["samples"]), 1),
+            )
+            labels.extend(return_matrix)
+
+        # Build Q-learning Design Matrix
+        samples_df = (
+            pd.DataFrame.from_records(samples, columns=sorted(samples[0].keys()))
+            .astype("float64")
+            .add_prefix("F_")
+        )
+        board_tensors_df = (
+            pd.DataFrame(board_tensors).astype("float64").add_prefix("BT_")
+        )
+        actions_df = pd.DataFrame(actions, columns=["ACTION"]).astype("int")
+        rewards_df = pd.DataFrame(
+            labels,
+            columns=[
+                "RETURN",
+                "DISCOUNTED_RETURN",
+                "TOURNAMENT_RETURN",
+                "DISCOUNTED_TOURNAMENT_RETURN",
+                "VICTORY_POINTS_RETURN",
+            ],
+        ).astype("float64")
+        main_df = pd.concat(
+            [samples_df, board_tensors_df, actions_df, rewards_df], axis=1
+        )
+
+        print(
+            "Collected DataFrames. Data size:",
+            "Main:",
+            main_df.shape,
+            "Samples:",
+            samples_df.shape,
+            "Board Tensors:",
+            board_tensors_df.shape,
+            "Actions:",
+            actions_df.shape,
+            "Rewards:",
+            rewards_df.shape,
+        )
+        populate_matrices(
+            samples_df,
+            board_tensors_df,
+            actions_df,
+            rewards_df,
+            main_df,
+            self.output,
+        )
+        print(
+            "Saved to matrices at:",
+            self.output,
+            ". Took",
+            formatSecs(time.time() - t1),
+        )
+        return samples_df, board_tensors_df, actions_df, rewards_df
---
+++ catanatron_experimental/catanatron_experimental/cli/cli_players.py
@@ -0,0 +1,110 @@
+from collections import namedtuple
+
+from rich.table import Table
+
+from catanatron.models.player import RandomPlayer
+from catanatron.players.weighted_random import WeightedRandomPlayer
+from catanatron_experimental.my_player import MyPlayer
+from catanatron_experimental.mcts_score_collector import (
+    MCTSScoreCollector,
+    MCTSPredictor,
+)
+from catanatron_experimental.machine_learning.players.reinforcement import (
+    QRLPlayer,
+    TensorRLPlayer,
+    VRLPlayer,
+    PRLPlayer,
+)
+from catanatron_experimental.tensorforce_player import ForcePlayer
+from catanatron_experimental.machine_learning.players.minimax import (
+    AlphaBetaPlayer,
+    ValueFunctionPlayer,
+)
+from catanatron.players.search import (
+    VictoryPointPlayer,
+)
+from catanatron_experimental.machine_learning.players.mcts import MCTSPlayer
+from catanatron_experimental.machine_learning.players.scikit import ScikitPlayer
+from catanatron_experimental.machine_learning.players.playouts import (
+    GreedyPlayoutsPlayer,
+)
+from catanatron_experimental.machine_learning.players.online_mcts_dqn import (
+    OnlineMCTSDQNPlayer,
+)
+from catanatron_experimental.dqn_player import DQNPlayer
+
+# PLAYER_CLASSES = {
+#     "O": OnlineMCTSDQNPlayer,
+#     "S": ScikitPlayer,
+#     "Y": MyPlayer,
+#     # Used like: --players=V:path/to/model.model,T:path/to.model
+#     "C": ForcePlayer,
+#     "VRL": VRLPlayer,
+#     "Q": QRLPlayer,
+#     "P": PRLPlayer,
+#     "T": TensorRLPlayer,
+#     "D": DQNPlayer,
+#     "CO": MCTSScoreCollector,
+#     "COP": MCTSPredictor,
+# }
+
+# Player must have a CODE, NAME, DESCRIPTION, CLASS.
+CliPlayer = namedtuple("CliPlayer", ["code", "name", "description", "import_fn"])
+CLI_PLAYERS = [
+    CliPlayer("R", "RandomPlayer", "Chooses actions at random.", RandomPlayer),
+    CliPlayer(
+        "W",
+        "WeightedRandomPlayer",
+        "Like RandomPlayer, but favors buying cities, settlements, and dev cards when possible.",
+        WeightedRandomPlayer,
+    ),
+    CliPlayer(
+        "VP",
+        "VictoryPointPlayer",
+        "Chooses randomly from actions that increase victory points immediately if possible, else at random.",
+        VictoryPointPlayer,
+    ),
+    CliPlayer(
+        "G",
+        "GreedyPlayoutsPlayer",
+        "For each action, will play N random 'playouts'. "
+        + "Takes the action that led to best winning percent. "
+        + "First param is NUM_PLAYOUTS",
+        GreedyPlayoutsPlayer,
+    ),
+    CliPlayer(
+        "M",
+        "MCTSPlayer",
+        "Decides according to the MCTS algorithm. First param is NUM_SIMULATIONS.",
+        MCTSPlayer,
+    ),
+    CliPlayer(
+        "F",
+        "ValueFunctionPlayer",
+        "Chooses the action that leads to the most immediate reward, based on a hand-crafted value function.",
+        ValueFunctionPlayer,
+    ),
+    CliPlayer(
+        "AB",
+        "AlphaBetaPlayer",
+        "Implements alpha-beta algorithm. That is, looks ahead a couple "
+        + "levels deep evaluating leafs with hand-crafted value function. "
+        + "Params are DEPTH, PRUNNING",
+        AlphaBetaPlayer,
+    ),
+    CliPlayer(
+        "Y",
+        "MyPlayer",
+        "Uses catanatron_experimental/catanatron_experimental/my_player.py. "
+        + "Edit this file with your own strategy and test it out here!",
+        MyPlayer,
+    ),
+]
+
+
+player_help_table = Table(title="Player Legend")
+player_help_table.add_column("CODE", justify="center", style="cyan", no_wrap=True)
+player_help_table.add_column("PLAYER")
+player_help_table.add_column("DESCRIPTION")
+for player in CLI_PLAYERS:
+    player_help_table.add_row(player.code, player.name, player.description)
