c2755a68d4289fbd7f69f0063720afcd93b66848
Bryan Collazo <bcollazo2010@gmail.com>
2022-03-06 07:46:07 -0400

    Fix action_from_json bug

--- tests/test_json.py
+++ tests/test_json.py
@@ -1,8 +1,9 @@
 import json
 
 from catanatron.game import Game
+from catanatron.models.enums import ActionType
 from catanatron.models.player import SimplePlayer, Color
-from catanatron.json import GameEncoder
+from catanatron.json import GameEncoder, action_from_json
 
 
 def test_serialization():
@@ -24,3 +25,11 @@ def test_serialization():
     assert isinstance(result["edges"], list)
     assert isinstance(result["nodes"], dict)
     assert isinstance(result["actions"], list)
+
+
+def test_action_from_json():
+    data = ["RED", "MARITIME_TRADE", ["SHEEP", "SHEEP", "SHEEP", "SHEEP", "ORE"]]
+    action = action_from_json(data)
+    assert action.color == Color.RED
+    assert action.action_type == ActionType.MARITIME_TRADE
+    assert action.value == ("SHEEP", "SHEEP", "SHEEP", "SHEEP", "ORE")
