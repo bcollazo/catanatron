e655ce24467d49145ab58d408c9446c52abe9632
Bryan Collazo <bcollazo2010@gmail.com>
2022-05-12 07:14:31 -0400

    Remove tensorflow from catanatron_gym dependencies

--- catanatron_gym/setup.py
+++ catanatron_gym/setup.py
@@ -22,5 +22,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
-    install_requires=["catanatron", "gym", "tensorflow"],
+    install_requires=["catanatron", "gym"],
 )
