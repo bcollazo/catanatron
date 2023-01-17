050384a67847e037ee5ba96e2d129e589d711578
Fernando Yordan <7763689+fyordan@users.noreply.github.com>
2023-01-17 07:03:17 -0400

    workflow and performance tests for github actions (#246)
    * workflow and performance tests for github actions
    
    * make regression tests go immediately after the python build
    
    * fix each step should have atleast one users or run
    
    * Address comments
    
    Co-authored-by: Fernando Yordan <fernando@Fernandos-MacBook-Pro.local>

--- tests/integration_tests/test_speed.py
+++ tests/integration_tests/test_speed.py
@@ -1,9 +1,13 @@
 import json
 
-from catanatron.json import GameEncoder
 from catanatron.game import Game
-from catanatron.models.player import SimplePlayer, Color
+from catanatron.json import GameEncoder
+from catanatron.models.player import Color, SimplePlayer, RandomPlayer
+from catanatron.players.weighted_random import WeightedRandomPlayer
 from catanatron_gym.features import create_sample
+from catanatron_experimental.machine_learning.players.minimax import AlphaBetaPlayer, SameTurnAlphaBetaPlayer
+
+RANDOM_SEED = 0
 
 
 # Things to benchmark. create_sample(), game.play() (random game), .to_json(), .copy()
@@ -14,7 +18,7 @@ def test_to_json_speed(benchmark):
         SimplePlayer(Color.ORANGE),
         SimplePlayer(Color.WHITE),
     ]
-    game = Game(players)
+    game = Game(players, seed=RANDOM_SEED)
 
     result = benchmark(json.dumps, game, cls=GameEncoder)
     assert isinstance(result, str)
@@ -27,7 +31,7 @@ def test_copy_speed(benchmark):
         SimplePlayer(Color.ORANGE),
         SimplePlayer(Color.WHITE),
     ]
-    game = Game(players)
+    game = Game(players, seed=RANDOM_SEED)
 
     result = benchmark(game.copy)
     assert result.seed == game.seed
@@ -40,10 +44,75 @@ def test_create_sample_speed(benchmark):
         SimplePlayer(Color.WHITE),
         SimplePlayer(Color.ORANGE),
     ]
-    game = Game(players)
+    game = Game(players, seed=RANDOM_SEED)
     for _ in range(30):
         game.play_tick()
 
     sample = benchmark(create_sample, game, players[1].color)
     assert isinstance(sample, dict)
     assert len(sample) > 0
+
+
+# Benchmarking individual player speeds
+def test_simpleplayer_speed(benchmark):
+    players = [
+        SimplePlayer(Color.RED),
+        SimplePlayer(Color.BLUE),
+        SimplePlayer(Color.WHITE),
+        SimplePlayer(Color.ORANGE),
+    ]
+    game = Game(players, seed=RANDOM_SEED)
+    def _play_game(game):
+        for _ in range(100):
+            game.play_tick()
+        return game
+
+    result = benchmark(_play_game, game)
+
+
+def test_weightedrandom_speed(benchmark):
+    players = [
+        RandomPlayer(Color.RED),
+        RandomPlayer(Color.BLUE),
+        RandomPlayer(Color.WHITE),
+        WeightedRandomPlayer(Color.ORANGE),
+    ]
+    game = Game(players, seed=RANDOM_SEED)
+    def _play_game(game):
+        for _ in range(100):
+            game.play_tick()
+        return game
+
+    result = benchmark(_play_game, game)
+
+
+def test_alphabeta_speed(benchmark):
+    players = [
+        RandomPlayer(Color.RED),
+        RandomPlayer(Color.BLUE),
+        RandomPlayer(Color.WHITE),
+        AlphaBetaPlayer(Color.ORANGE),
+    ]
+    game = Game(players, seed=RANDOM_SEED)
+    def _play_game(game):
+        for _ in range(100):
+            game.play_tick()
+        return game
+
+    result = benchmark(_play_game, game)
+
+
+def test_same_turn_alphabeta_speed(benchmark):
+    players = [
+        RandomPlayer(Color.RED),
+        RandomPlayer(Color.BLUE),
+        RandomPlayer(Color.WHITE),
+        SameTurnAlphaBetaPlayer(Color.ORANGE),
+    ]
+    game = Game(players, seed=RANDOM_SEED)
+    def _play_game(game):
+        for _ in range(100):
+            game.play_tick()
+        return game
+
+    result = benchmark(_play_game, game)
