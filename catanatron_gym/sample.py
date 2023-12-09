4a2b4f8c359b4f7991200d4096190154934a78e4
Bryan Collazo <bcollazo2010@gmail.com>
2023-12-09 12:20:04 -0400

    Sample.py

--- catanatron_gym/sample.py
+++ catanatron_gym/sample.py
@@ -1,13 +1,12 @@
 import random
 import gymnasium as gym
 
-env = gym.make("catanatron_gym:catanatron-v0")
+
+env = gym.make("catanatron_gym:catanatron-v1")
 observation, info = env.reset()
 for _ in range(1000):
-    action = random.choice(
-        env.get_valid_actions()
-    )  # your agent here (this takes random actions)
-
+    # your agent here (this takes random actions)
+    action = random.choice(env.unwrapped.get_valid_actions())
     observation, reward, done, info = env.step(action)
     if done:
         observation, info = env.reset()
