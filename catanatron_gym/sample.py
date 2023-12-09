a63bae5de84e724e1451a6e8f144728422e979a7
Bryan Collazo <bcollazo2010@gmail.com>
2023-12-09 12:22:21 -0400

    Update sample.pr

--- catanatron_gym/sample.py
+++ catanatron_gym/sample.py
@@ -7,7 +7,8 @@ observation, info = env.reset()
 for _ in range(1000):
     # your agent here (this takes random actions)
     action = random.choice(env.unwrapped.get_valid_actions())
-    observation, reward, done, info = env.step(action)
+    observation, reward, terminated, truncated, info = env.step(action)
+    done = terminated or truncated
     if done:
         observation, info = env.reset()
 env.close()
