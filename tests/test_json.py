cd2939921fcef6c6060578abf3fd6780b899d797
Mason Zarns <mason@zarns.net>
2024-07-21 12:53:40 -0500

    Use resource enums in test

--- tests/test_json.py
+++ tests/test_json.py
@@ -2,7 +2,7 @@ import pytest
 import json
 
 from catanatron.game import Game
-from catanatron.models.enums import ActionType
+from catanatron.models.enums import ActionType, WOOD, BRICK, SHEEP, ORE
 from catanatron.models.player import SimplePlayer, Color
 from catanatron.json import GameEncoder, action_from_json
 
@@ -29,31 +29,31 @@ def test_serialization():
 
 
 def test_action_from_json_maritime_trade():
-    data = ["RED", "MARITIME_TRADE", ["SHEEP", "SHEEP", "SHEEP", "SHEEP", "ORE"]]
+    data = ["RED", "MARITIME_TRADE", [SHEEP, SHEEP, SHEEP, SHEEP, ORE]]
     action = action_from_json(data)
     assert action.color == Color.RED
     assert action.action_type == ActionType.MARITIME_TRADE
-    assert action.value == ("SHEEP", "SHEEP", "SHEEP", "SHEEP", "ORE")
+    assert action.value == (SHEEP, SHEEP, SHEEP, SHEEP, ORE)
 
 
 def test_action_from_json_play_year_of_plenty_two_resources():
-    data = ["RED", "PLAY_YEAR_OF_PLENTY", ["WOOD", "BRICK"]]
+    data = ["RED", "PLAY_YEAR_OF_PLENTY", [WOOD, BRICK]]
     action = action_from_json(data)
     assert action.color == Color.RED
     assert action.action_type == ActionType.PLAY_YEAR_OF_PLENTY
-    assert action.value == ("WOOD", "BRICK")
+    assert action.value == (WOOD, BRICK)
 
 
 def test_action_from_json_play_year_of_plenty_one_resource():
-    data = ["BLUE", "PLAY_YEAR_OF_PLENTY", ["SHEEP"]]
+    data = ["BLUE", "PLAY_YEAR_OF_PLENTY", [SHEEP]]
     action = action_from_json(data)
     assert action.color == Color.BLUE
     assert action.action_type == ActionType.PLAY_YEAR_OF_PLENTY
-    assert action.value == ("SHEEP",)
+    assert action.value == (SHEEP,)
 
 
 def test_action_from_json_play_year_of_plenty_invalid():
-    data = ["WHITE", "PLAY_YEAR_OF_PLENTY", ["WOOD", "BRICK", "SHEEP"]]
+    data = ["WHITE", "PLAY_YEAR_OF_PLENTY", [WOOD, BRICK, SHEEP]]
     with pytest.raises(
         ValueError, match="Year of Plenty action must have 1 or 2 resources"
     ):
