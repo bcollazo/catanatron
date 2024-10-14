e90f64d9b123e4b2a2e85edc6a8b2e48ce91319f
Bryan Collazo <bcollazo2010@gmail.com>
2024-10-14 07:47:29 -0400

    Copy Benchmark Update

--- catanatron_rust/benches/copy_benchmark.rs
+++ catanatron_rust/benches/copy_benchmark.rs
@@ -1,45 +1,72 @@
+use catanatron_rust::decks::{DevCardDeck, ResourceDeck};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use rand::Rng; // For random number generation
-
-fn copy_array_benchmark(c: &mut Criterion) {
-    // Initialize an array of length 1200
-    let array = [0u8; 1200];
-
-    c.bench_function("Copy 1200-length array", |b| {
-        b.iter(|| {
-            let mut rng = rand::thread_rng();
-            // Copy the array (deep copy)
-            let mut copied_array = black_box(array);
-            // Generate a random index between 0 and 1199
-            let random_index = rng.gen_range(0..1200);
-            // Generate a random value between 0 and 255 (u8 range)
-            let random_value = rng.gen_range(0..=255);
-            // Modify the array at the random index
-            copied_array[random_index] = random_value;
-            black_box(copied_array);
-        })
-    });
-}
 
 fn copy_vector_benchmark(c: &mut Criterion) {
     // Initialize a vector of length 600
     let vector = vec![0u8; 600];
 
     c.bench_function("Clone 600-length vector", |b| {
+        let mut iteration_number = 0;
         b.iter(|| {
-            let mut rng = rand::thread_rng();
-            // Clone the vector (deep copy)
-            let mut copied_vector = black_box(vector.clone());
-            // Generate a random index between 0 and 599
-            let random_index = rng.gen_range(0..600);
-            // Generate a random value between 0 and 255 (u8 range)
-            let random_value = rng.gen_range(0..=255);
-            // Modify the vector at the random index
-            copied_vector[random_index] = random_value;
+            let mut copied_vector = vector.clone();
+            let index = iteration_number % 600;
+            let random_value = (iteration_number % 256) as u8;
+            copied_vector[index] = random_value;
             black_box(copied_vector);
+
+            iteration_number += 1;
         })
     });
 }
 
-criterion_group!(benches, copy_array_benchmark, copy_vector_benchmark);
+fn copy_array_benchmark(c: &mut Criterion) {
+    // Initialize an array of length 1200
+    let array = [0u8; 600];
+
+    c.bench_function("Copy 600-length array", |b| {
+        let mut iteration_number = 0;
+        b.iter(|| {
+            let mut copied_array = array;
+            let index = iteration_number % 600;
+            let random_value = (iteration_number % 256) as u8;
+            copied_array[index] = random_value;
+            black_box(copied_array);
+
+            iteration_number += 1;
+        })
+    });
+}
+
+#[derive(Debug, Clone)]
+pub struct State {
+    pub bank_resources: ResourceDeck,
+    pub bank_development_cards: DevCardDeck,
+}
+
+impl State {
+    pub fn initialize_state() -> State {
+        State {
+            bank_resources: ResourceDeck::starting_resource_bank(),
+            bank_development_cards: DevCardDeck::starting_deck(),
+        }
+    }
+}
+
+fn copy_struct_benchmark(c: &mut Criterion) {
+    let state = State::initialize_state();
+
+    c.bench_function("Clone Struct1", |b| {
+        b.iter(|| {
+            let copied_struct1 = black_box(state.clone());
+            black_box(copied_struct1);
+        })
+    });
+}
+
+criterion_group!(
+    benches,
+    copy_vector_benchmark,
+    copy_array_benchmark,
+    copy_struct_benchmark
+);
 criterion_main!(benches);
